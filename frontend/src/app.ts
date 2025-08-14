2127b068f7058e66480d5f4bfe54e50b0981b85c
Ellen Jiang <ellenj@google.com>
2025-08-14 17:55:11 -0400

    Add banner component and global service

--- frontend/src/app.ts
+++ frontend/src/app.ts
@@ -24,17 +24,21 @@ import "./components/settings/project_dialog";
 import "./components/floating_panel_host/floating_panel_host";
 import "./components/smart_highlight_menu/smart_highlight_menu";
 import "./components/dialogs/dialogs";
+import "./components/banner/banner";
 import "lit-toast/lit-toast.js";
 
 import { MobxLitElement } from "@adobe/lit-mobx";
 import { CSSResultGroup, html, nothing, TemplateResult } from "lit";
 import { customElement } from "lit/decorators.js";
 import { createRef, ref, Ref } from "lit/directives/ref.js";
+import { styleMap } from "lit/directives/style-map.js";
 
 import { core } from "./core/core";
 import { Pages, RouterService } from "./services/router.service";
 import { SettingsService } from "./services/settings.service";
 import { SnackbarService } from "./services/snackbar.service";
+import { BannerService } from "./services/banner.service";
+import { BANNER_HEIGHT } from "./components/banner/banner";
 
 import { styles } from "./app.scss";
 import { LightMobxLitElement } from "./components/light_mobx_lit_element/light_mobx_lit_element";
@@ -47,6 +51,7 @@ export class App extends LightMobxLitElement {
   private readonly routerService = core.getService(RouterService);
   private readonly settingsService = core.getService(SettingsService);
   private readonly snackbarService = core.getService(SnackbarService);
+  private readonly bannerService = core.getService(BannerService);
 
   private readonly toastRef: Ref<any> = createRef<any>();
 
@@ -96,13 +101,38 @@ export class App extends LightMobxLitElement {
     return html`<div class="content">404: ${message}</div>`;
   }
 
+  private renderBanner() {
+    if (!this.bannerService.isBannerOpen) {
+      return nothing;
+    }
+    const bannerProperties = this.bannerService.getBannerProperties()!;
+
+    return html`
+      <lumi-banner
+        .text=${bannerProperties.message}
+        .icon=${bannerProperties.icon}
+        .actionText=${bannerProperties.actionText}
+        .actionCallback=${bannerProperties.actionCallback}
+        .onBannerClose=${bannerProperties.closeCallback}
+      >
+      </lumi-banner>
+    `;
+  }
+
   override render() {
+    const mainStyles = styleMap({
+      "height": this.bannerService.isBannerOpen
+        ? `calc(100% - ${BANNER_HEIGHT}px)`
+        : "100%",
+    });
+
     return html`
       <style>
         ${styles}
       </style>
       <div class="app-wrapper mode--${this.settingsService.colorMode}">
-        <main>
+        ${this.renderBanner()}
+        <main style=${mainStyles}>
           <div class="content-wrapper">${this.renderPageContent()}</div>
           <project-dialog></project-dialog>
           <floating-panel-host></floating-panel-host>
